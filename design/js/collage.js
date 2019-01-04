a456ee56ba3d7e970f8266056ce6ea328db3ecbf
j55-webdev <46341803+j55-webdev@users.noreply.github.com>
2019-01-04 12:00:50 +0200

    Create collage.js for my-project Github Pages

---
+++ design/js/collage.js
@@ -0,0 +1,128 @@
+/* First Block */
+$('#firstBlock').mouseover(function(){
+	$('#firstBlock').css('background', 'white');
+	$('#firstText').css('display', 'unset');
+	return;
+});
+$('#firstBlock').mouseout(function(){
+	$('#firstBlock').css('background', 'url("https://i.imgur.com/Rui0hj8.jpg")');
+	$('#firstBlock').css('background-repeat', 'no-repeat');
+	$('#firstBlock').css('background-size', '100%');
+	$('#firstText').css('display', 'none');
+	return;
+});
+
+/* Second Block */
+$('#secondBlock').mouseover(function(){
+	$('#secondBlock').css('background', 'white');
+	$('#secondText').css('display', 'unset');
+	return;
+});
+$('#secondBlock').mouseout(function(){
+	$('#secondBlock').css('background', 'url("https://i.imgur.com/FQvV0bz.png")');
+	$('#secondBlock').css('background-repeat', 'no-repeat');
+	$('#secondBlock').css('background-size', '100%');
+	$('#secondText').css('display', 'none');
+	return;
+});
+
+/* Third Block */
+$('#thirdBlock').mouseover(function(){
+	$('#thirdBlock').css('background', 'white');
+	$('#thirdText').css('display', 'unset');
+	return;
+});
+$('#thirdBlock').mouseout(function(){
+	$('#thirdBlock').css('background', 'url("https://i.imgur.com/CsOIfMrl.jpg")');
+	$('#thirdBlock').css('background-repeat', 'no-repeat');
+	$('#thirdBlock').css('background-size', '100%');
+	$('#thirdText').css('display', 'none');
+	return;
+});
+
+/* Fourth Block */
+$('#fourthBlock').mouseover(function(){
+	$('#fourthBlock').css('background', 'white');
+	$('#fourthText').css('display', 'unset');
+	return;
+});
+$('#fourthBlock').mouseout(function(){
+	$('#fourthBlock').css('background', 'url("https://i.imgur.com/85cpBXx.png")');
+	$('#fourthBlock').css('background-repeat', 'no-repeat');
+	$('#fourthBlock').css('background-size', '100%');
+	$('#fourthText').css('display', 'none');
+	return;
+});
+
+/*Fifth Block */
+$('#fifthBlock').mouseover(function(){
+	$('#fifthBlock').css('background', 'white');
+	$('#fifthText').css('display', 'unset');
+	return;
+});
+$('#fifthBlock').mouseout(function(){
+	$('#fifthBlock').css('background', 'url("https://i.imgur.com/AcdXFcKl.jpg")');
+	$('#fifthBlock').css('background-repeat', 'no-repeat');
+	$('#fifthBlock').css('background-size', '100%');
+	$('#fifthText').css('display', 'none');
+	return;
+});
+
+/*Sixth Block */
+$('#sixthBlock').mouseover(function(){
+	$('#sixthBlock').css('background', 'white');
+	$('#sixthText').css('display', 'unset');
+	return;
+});
+$('#sixthBlock').mouseout(function(){
+	$('#sixthBlock').css('background', 'url("https://i.imgur.com/VLif7PR.png")');
+	$('#sixthBlock').css('background-repeat', 'no-repeat');
+	$('#sixthBlock').css('background-size', '100%');
+	$('#sixthText').css('display', 'none');
+	return;
+});
+
+/*Seventh Block */
+$('#seventhBlock').mouseover(function(){
+	$('#seventhBlock').css('background', 'white');
+	$('#seventhText').css('display', 'unset');
+	return;
+});
+$('#seventhBlock').mouseout(function(){
+	$('#seventhBlock').css('background', 'url("https://i.imgur.com/AqaTrmi.png")');
+	$('#seventhBlock').css('background-repeat', 'no-repeat');
+	$('#seventhBlock').css('background-size', '100%');
+	$('#seventhText').css('display', 'none');
+	return;
+});
+
+/*Eigth Block */
+$('#eightBlock').mouseover(function(){
+	$('#eightBlock').css('background', 'white');
+	$('#eightText').css('display', 'unset');
+	return;
+});
+$('#eightBlock').mouseout(function(){
+	$('#eightBlock').css('background', 'url("https://i.imgur.com/qWSucWZ.jpg")');
+	$('#eightBlock').css('background-repeat', 'no-repeat');
+	$('#eightBlock').css('background-size', '100%');
+	$('#eightText').css('display', 'none');
+	return;
+});
+
+/*Ninth Block */
+$('#ninthBlock').mouseover(function(){
+	$('#ninthBlock').css('background', 'white');
+	$('#ninthText').css('display', 'unset');
+	return;
+});
+$('#ninthBlock').mouseout(function(){
+	$('#ninthBlock').css('background', 'url("https://i.imgur.com/rX93QvK.png")');
+	$('#ninthBlock').css('background-repeat', 'no-repeat');
+	$('#ninthBlock').css('background-size', '100%');
+	$('#ninthText').css('display', 'none');
+	return;
+});
+
+
+
